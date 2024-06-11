c9c7974754ebae1c2b7fb05bfbc55e274dc62fdd
skrskr <skrskrss@126.com>
2024-06-11 16:38:59 +0800

    updata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,12 @@ export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: ['@nuxt-themes/docus'],
   devtools: { enabled: true },
-
+  css: [  
+    './assets/main.css' // 路径前的 '@' 表示 `src/` 目录  
+  ],
   modules: [
     // Remove it if you don't use Plausible analytics
     // https://github.com/nuxt-modules/plausible
     '@nuxtjs/plausible'
-  ],
-  routeRules: {
-    '/': { redirect: '/introduction/getting-started', prerender: true }
-  },
+  ]
 })
