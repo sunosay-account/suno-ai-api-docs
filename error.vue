4bfe9b8330d2feba7d87f9e906223c6d528b514a
skrskr <skrskrss@126.com>
2024-08-29 11:49:59 +0800

    update

---
+++ error.vue
@@ -0,0 +1,13 @@
+
+<template>
+  <div class="grid h-screen place-content-center bg-white px-4 gap-10">
+    
+    <h1 class="uppercase tracking-widest text-gray-500 font-bold font-sans">
+      404 | Pages Not Found
+    </h1>
+    <a href="/" class="mx-auto uppercase font-sans font-bold tracking-[0.2rem] rounded-full text-sm gap-x-3 px-6 py-3 shadow-sm text-white dark:text-gray-900 bg-gray-900 hover:bg-gray-800  dark:bg-white dark:hover:bg-gray-100 dark:disabled:bg-white f inline-flex items-center max-w-fit">
+      <!-- <Icon name="i-heroicons-home" class=" flex-shrink-0 h-5 w-5"></Icon> -->
+      <span>Home</span>
+    </a>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,9 +5,32 @@ export default defineNuxtConfig({
   css: [  
     './assets/main.css'
   ],
+  routeRules: {
+    '/**' : {robots: true}
+  },
   modules: [
     // Remove it if you don't use Plausible analytics
     // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible'
-  ]
+    '@nuxtjs/plausible',
+    '@nuxtjs/sitemap',
+    '@nuxtjs/robots'
+    
+  ],
+  robots: {
+    sitemap: ['sitemap.xml'],
+    disallow: ['music/_dir','_nuxt/']
+  },
+  ssr: true,
+  sourcemap: false,
+  site: {
+    url: 'https://docs.sunoapi.co',
+    trailingSlash: false,
+    name: 'Suno APi Docs',
+  },
+  sitemap: {
+    enabled: true,
+    xsl: false,
+    xslTips: false,
+    discoverImages: false,
+  },
 })
