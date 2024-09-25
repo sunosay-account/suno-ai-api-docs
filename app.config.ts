12fba45869aff67f5428b89ab6584e95bb5227d1
skrskr <skrskrss@126.com>
2024-09-25 11:11:03 +0800

    update

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'SunoAPI Docs',
+    title: 'Suno API Documentation',
     description: 'Welcome to the SunoAPI documentation, your best starting point for understanding and utilizing our music and lyric generation interfaces. Here, you will find detailed usage guides, sample code, and FAQs to help you get started easily and make the most of our API features.',
     image: 'favicon.ico',
     aside: {
@@ -15,7 +15,7 @@ export default defineAppConfig({
     },
     header: {
       logo: false,
-      title: 'Suno API Docs',
+      title: 'Suno API Documentation',
       showLinkIcon: true,
       exclude: [],
       fluid: true
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,12 @@ export default defineNuxtConfig({
     name: 'SunoAPI Docs',
   },
   app: {
+    title: 'SunoAPI Suno API Documentation', // 设置全局默认标题
+      meta: [
+        { hid: 'description', name: 'description', content: 'Welcome to the SunoAPI documentation, your best starting point for understanding and utilizing our music and lyric generation interfaces. Here, you will find detailed usage guides, sample code, and FAQs to help you get started easily and make the most of our API features.' }, // 设置全局默认描述
+        { hid: 'charset', charset: 'utf-8' },
+        { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
     head: {
       script: [
         {
