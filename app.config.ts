aebc630636353671f578ae0c156920ac4169296b
skrskr <skrskrss@126.com>
2024-09-25 11:04:08 +0800

    update

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@
 export default defineAppConfig({
   docus: {
     title: 'SunoAPI Docs',
-    description: 'The best place to start your documentation.',
+    description: 'Welcome to the SunoAPI documentation, your best starting point for understanding and utilizing our music and lyric generation interfaces. Here, you will find detailed usage guides, sample code, and FAQs to help you get started easily and make the most of our API features.',
     image: 'favicon.ico',
     aside: {
       level: 0,
